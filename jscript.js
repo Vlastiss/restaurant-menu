e3cf1feb88f37f4ec42fd6e8b4fe0a563be279c3
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-14 21:00:44 +0000

    final version

--- jscript.js
+++ jscript.js
@@ -1,8 +1,54 @@
 $(document).ready(function () {
-    
 
-    
+    var collumIds = ['#lunchCol1', '#lunchCol2', '#breakfastCol1', '#breakfastCol2', '#dinnerCol1', '#dinnerCol2', '#shakeCol1','#shakeCol2']
 
+    $("#all").click(function() {
+        collumIds.forEach( id => {
+            if ($(id).hasClass("d-none")) {
+                $(id).removeClass("d-none")
+            }
+        })
+    })
+
+    $("#breakfast").click(function() {
+        removeClass();
+        collumIds.forEach( id => {
+            if (!$(id).is('#breakfastCol1, #breakfastCol2')) {
+                $(id).addClass("d-none")
+            }
+        })
+    })
+    
+    $("#lunch").click(function() {
+        removeClass();
+        collumIds.forEach( id => {
+            if (!$(id).is('#lunchCol1, #lunchCol2')) {
+                $(id).addClass("d-none")
+            }
+        })
+    })
+    $("#shakes").click(function() {
+        removeClass();
+        collumIds.forEach( id => {
+            if (!$(id).is('#shakeCol1, #shakeCol2')) {
+                $(id).addClass("d-none")
+            }
+        })
+    })
+    $("#dinner").click(function() {
+        removeClass();
+        collumIds.forEach( id => {
+            if (!$(id).is('#dinnerCol1, #dinnerCol2')) {
+                $(id).addClass("d-none")
+            }
+        })
+    })
+
+    function removeClass() {
+        collumIds.forEach( id => {
+                $(id).removeClass("d-none")
+        })
+    }
 
 
 
